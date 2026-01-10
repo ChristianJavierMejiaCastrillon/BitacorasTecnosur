2f87145fcac92168cc6eda8cbeb156f6ea469602
Christian Mejia <christianjaviermejia@hotmail.com>
2026-01-09 19:28:55 -0500

    se crean las clases BitacoraDAL y NovedadDAL

---
+++ BitacorasWeb/Datos/BitacoraDAL.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace BitacorasWeb.Datos
+{
+    public class BitacoraDAL
+    {
+        public int CrearBitacora(DateTime fecha, string turno, int idMaquina, int idUsuario)
+        {
+            const string sql = @"
+                INSERT INTO Bitacora (Fecha, HoraInicio, HoraFin, Turno, IdMaquina, IdUsuario)
+                VALUES (@Fecha, NULL, NULL, @Turno, @IdMaquina, @IdUsuario);
+
+                SELECT SCOPE_IDENTITY();";
+
+            using (SqlConnection conexion = ConexionBD.CrearConexion())
+            using (SqlCommand comando = new SqlCommand(sql, conexion))
+            {
+                comando.Parameters.Add("@Fecha", SqlDbType.Date).Value = fecha.Date;
+                comando.Parameters.Add("@Turno", SqlDbType.NVarChar, 20).Value = turno;
+                comando.Parameters.Add("@IdMaquina", SqlDbType.Int).Value = idMaquina;
+                comando.Parameters.Add("@IdUsuario", SqlDbType.Int).Value = idUsuario;
+
+                conexion.Open();
+                object result = comando.ExecuteScalar();
+
+                return Convert.ToInt32(result);
+            }
+        }
+    }
+}
---
+++ BitacorasWeb/Datos/NovedadDAL.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace BitacorasWeb.Datos
+{
+    public class NovedadDAL
+    {
+        public void InsertarNovedad(int idBitacora, int idProducto, string tipo, string descripcion, string reportadoPor)
+        {
+            const string sql = @"
+                INSERT INTO Novedad (IdBitacora, Tipo, Descripcion, ReportadoPor, Validado, IdProducto)
+                VALUES (@IdBitacora, @Tipo, @Descripcion, @ReportadoPor, 0, @IdProducto);";
+
+            using (SqlConnection conexion = ConexionBD.CrearConexion())
+            using (SqlCommand comando = new SqlCommand(sql, conexion))
+            {
+                comando.Parameters.Add("@IdBitacora", SqlDbType.Int).Value = idBitacora;
+                comando.Parameters.Add("@Tipo", SqlDbType.NVarChar, 50).Value = tipo;
+                comando.Parameters.Add("@Descripcion", SqlDbType.NVarChar, 500).Value = descripcion;
+                comando.Parameters.Add("@ReportadoPor", SqlDbType.NVarChar, 150).Value =
+                    (object)reportadoPor ?? DBNull.Value;
+                comando.Parameters.Add("@IdProducto", SqlDbType.Int).Value = idProducto;
+
+                conexion.Open();
+                comando.ExecuteNonQuery();
+            }
+        }
+    }
+}
